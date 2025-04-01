b29228a7ff052c3cfa3d06331c6e0a7e7a2d7bdf
Chanchal <chanchalagnihotri1987@gmail.com>
2025-04-01 10:56:13 +0530

    Made api url changes.

--- MyTodo.Blazor.Client/Services/ApiClientsHandler.cs
+++ MyTodo.Blazor.Client/Services/ApiClientsHandler.cs
@@ -5,37 +5,70 @@
         public static void ConfigureApiClients(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddHttpClient<LifeAreaClient>(client =>
-                  client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["LifeArea"]));
+                  client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/lifearea-service");
 
             services.AddHttpClient<ProblemClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Problem"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/problem-service"));
 
             services.AddHttpClient<IMyTodoHttpClient<Problem>, ProblemClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Problem"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/problem-service"));
 
             services.AddHttpClient<GoalClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Goal"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/goal-service"));
 
             services.AddHttpClient<IMyTodoHttpClient<Goal>, GoalClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Goal"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/goal-service"));
 
             services.AddHttpClient<ObjectiveClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Objective"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/objective-service"));
 
             services.AddHttpClient<IMyTodoHttpClient<Objective>, ObjectiveClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Objective"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/objective-service"));
 
             services.AddHttpClient<TaskClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/todo-service"));
 
             services.AddHttpClient<IMyTodoHttpClient<TaskItem>, TaskClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/todo-service"));
 
             services.AddHttpClient<TodoClient>(client =>
-                client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+                client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/todo-service"));
 
             services.AddHttpClient<SprintClient>(client =>
-               client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Sprint"]));
+               client.BaseAddress = new Uri("https://mytodoapi-client.azurewebsites.net/sprint-service"));
+
+            //services.AddHttpClient<LifeAreaClient>(client =>
+            //      client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["LifeArea"]));
+
+            //services.AddHttpClient<ProblemClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Problem"]));
+
+            //services.AddHttpClient<IMyTodoHttpClient<Problem>, ProblemClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Problem"]));
+
+            //services.AddHttpClient<GoalClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Goal"]));
+
+            //services.AddHttpClient<IMyTodoHttpClient<Goal>, GoalClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Goal"]));
+
+            //services.AddHttpClient<ObjectiveClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Objective"]));
+
+            //services.AddHttpClient<IMyTodoHttpClient<Objective>, ObjectiveClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Objective"]));
+
+            //services.AddHttpClient<TaskClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+
+            //services.AddHttpClient<IMyTodoHttpClient<TaskItem>, TaskClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+
+            //services.AddHttpClient<TodoClient>(client =>
+            //    client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Task"]));
+
+            //services.AddHttpClient<SprintClient>(client =>
+            //   client.BaseAddress = new Uri(configuration.GetSection("ApiUrl")["Sprint"]));
         }
 
     }
